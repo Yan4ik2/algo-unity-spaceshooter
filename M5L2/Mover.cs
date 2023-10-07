676d68bba07bbbf2bcf6fc5bfb9a9a44c1666ac8
Adrian Josele Quional <13906452+ajgquional@users.noreply.github.com>
2023-10-07 09:48:29 +0800

    Create Mover.cs

---
+++ M5L2/Mover.cs
@@ -0,0 +1,38 @@
+/* **************************************************
+ * Author: 
+ *      Adrian Josele G. Quional
+ * 
+ * Program description: 
+ *      This program controls the operation of the 
+ *      obstacles and collectibles in the game.
+ * 
+ * How to use the script:
+ *      - Create the obstacle and colletible items
+ *      - Setup the necessary sprites
+ *      - Attach the script to the both objects
+ *      - Make a prefab out of the objects
+ * **************************************************/
+
+// libraries
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    public float speed = 2f;    // initial speed of the objects (can be modified via Inspector)
+
+    // just destroys the game object 5s after starting the scene
+    void Start()
+    {
+        // the game object which the script is attached to is deleted 5s after starting the scene
+        // ensures that the game object would be destroyed when it goes out of the screen
+        Destroy(gameObject, 5f);
+    }
+
+    // continuously move the objects down
+    void Update()
+    {
+        transform.position = (Vector2)transform.position + Vector2.down * (speed + GameController.gameSpeed) * Time.deltaTime;
+    }
+}
