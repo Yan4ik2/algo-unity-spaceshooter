5ce4bc0966a4df50728dd1d30383b18afd387d38
Adrian Josele Quional <13906452+ajgquional@users.noreply.github.com>
2023-10-07 09:56:25 +0800

    Create Obstacle.cs

---
+++ M5L2/Obstacle.cs
@@ -0,0 +1,29 @@
+/* **************************************************
+ * Author: 
+ *      Adrian Josele G. Quional
+ * 
+ * Program description: 
+ *      This program handles the collision between
+ *      the game object and the Player.
+ * 
+ * How to use the script:
+ *      - Attach the script to the object/s
+ * **************************************************/
+
+// libraries
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    // checks if the game object collides specifically with the Player
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // if the Player collides with an Obstacle, destroy the Player
+        if (collision.tag == "Player")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
